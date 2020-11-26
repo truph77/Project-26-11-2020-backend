c43b0a61e369ccd622ef37f61a1df6dafe8e3010
Trung Pham <phamtrung2422k@gmail.com>
2020-11-26 18:08:11 +0700

    first commit

--- handler/user.go
+++ handler/user.go
@@ -5,13 +5,27 @@ import (
 	"github.com/truph77/models"
 )
 
-//GetInfoUser used to get user info
-func GetInfoUser(c echo.Context) (err error) {
+//GetUser used to get user info
+func GetUser(c echo.Context) (err error) {
 	//Connect DB
 	// db, err := connectDB()
 	// if err != nil {
 	// 	return
 	// }
 	users := []models.User{}
+}
+
+//CreateUser ...
+func CreateUser(c echo.Context) (err error) {
+
+}
+
+//UpdateUser...
+func UpdateUser(c echo.Context) (err error) {
+
+}
+
+//DeleteUser ...
+func DeleteUser(c echo.Context) (err error) {
 
 }
--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"github.com/labstack/echo"
+	"github.com/truph77/handler"
+)
+
 func main() {
+	e := echo.New()
+
+	// Routes
+	e.POST("/users", handler.CreateUser)
+	e.GET("/users/:id", handler.GetUser)
+	e.PUT("/users/:id", handler.UpdateUser)
+	e.DELETE("/users/:id", handler.DeleteUser)
+
+	e.Logger.Fatal(e.Start(":7777"))
 }
